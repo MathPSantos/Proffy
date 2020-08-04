38c4aaff8f6cc6b88a562430f85708fc8a6a95f9
Matheus Santos <matheus.psantos2016@gmail.com>
2020-08-03 23:07:31 -0300

    feat: form filter added

--- web/src/pages/Landing/index.tsx
+++ web/src/pages/Landing/index.tsx
@@ -1,6 +1,5 @@
-import { rootCertificates } from "tls";
-
 import React from "react";
+import { Link } from "react-router-dom"
 
 import './styles.css';
 
@@ -17,7 +16,7 @@ const Landing: React.FC = () => {
       <div id="page-landing-content" className="container">
         <div className="logo-container">
           <img src={LogoImg} alt="Proffy Logo" />
-          <h2>Sua platforma de estudos online. </h2>
+          <h2>Sua plataforma de estudos online. </h2>
         </div>
 
         <img
@@ -27,19 +26,20 @@ const Landing: React.FC = () => {
         />
 
         <div className="buttons-container">
-            <a href="" className="study">
+            <Link to="/study" className="study">
                 <img src={StudyIcon} alt="Estudar"/>
                 Estudar
-            </a>
+            </Link>
 
-            <a href="" className="give-classes">
+            <Link to="/give-classes" className="give-classes">
                 <img src={GiveClassesIcon} alt="Dar Aulas"/>
                 Dar Aulas
-            </a>
+            </Link>
         </div>
 
         <span className="total-connections">
-            Total de 200 conexão já realizadas <img src={PurpleHeartIcon} alt="Coração roxo"/>
+          Total de 200 conexão já realizadas 
+          <img src={PurpleHeartIcon} alt="Coração roxo"/>
         </span>
       </div>
     </div>
