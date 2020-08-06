6ee0aed69bd44d08c03d327c766282d43f27ecc3
Matheus Santos <matheus.psantos2016@gmail.com>
2020-08-06 12:20:56 -0300

    feat: connection to api added

--- web/src/pages/Landing/index.tsx
+++ web/src/pages/Landing/index.tsx
@@ -1,7 +1,5 @@
-import React from "react";
-import { Link } from "react-router-dom"
-
-import './styles.css';
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
 import LogoImg from "../../assets/images/logo.svg";
 import LandingImg from "../../assets/images/landing.svg";
@@ -10,7 +8,21 @@ import StudyIcon from "../../assets/images/icons/study.svg";
 import GiveClassesIcon from "../../assets/images/icons/give-classes.svg";
 import PurpleHeartIcon from "../../assets/images/icons/purple-heart.svg";
 
+import api from "../../services/api";
+
+import "./styles.css";
+
 const Landing: React.FC = () => {
+  const [totalConnections, setTotalConnections] = useState(0);
+
+  useEffect(() => {
+    api.get("/connections").then((response) => {
+      const { total } = response.data;
+
+      setTotalConnections(total);
+    });
+  }, []);
+
   return (
     <div id="page-landing">
       <div id="page-landing-content" className="container">
@@ -26,20 +38,20 @@ const Landing: React.FC = () => {
         />
 
         <div className="buttons-container">
-            <Link to="/study" className="study">
-                <img src={StudyIcon} alt="Estudar"/>
-                Estudar
-            </Link>
+          <Link to="/study" className="study">
+            <img src={StudyIcon} alt="Estudar" />
+            Estudar
+          </Link>
 
-            <Link to="/give-classes" className="give-classes">
-                <img src={GiveClassesIcon} alt="Dar Aulas"/>
-                Dar Aulas
-            </Link>
+          <Link to="/give-classes" className="give-classes">
+            <img src={GiveClassesIcon} alt="Dar Aulas" />
+            Dar Aulas
+          </Link>
         </div>
 
         <span className="total-connections">
-          Total de 200 conexão já realizadas 
-          <img src={PurpleHeartIcon} alt="Coração roxo"/>
+          Total de {totalConnections} conexões já realizadas
+          <img src={PurpleHeartIcon} alt="Coração roxo" />
         </span>
       </div>
     </div>
