5f5c8c4a8d7f275e6a5265b429b0a9d94d9eee5e
Matheus Santos <matheus.psantos2016@gmail.com>
2020-08-06 12:12:49 -0300

    feat: Schedule fieldset started

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import PageHeader from "../../components/PageHeader";
 import Input from "../../components/Input";
@@ -10,6 +10,14 @@ import WarningIcon from "../../assets/images/icons/warning.svg";
 import "./styles.css";
 
 const TeacherForm: React.FC = () => {
+  const [scheduleItems, setScheduleItems] = useState([
+    { week_day: 0, from: "", to: "" },
+  ]);
+
+  function addNewScheduleItem() {
+    setScheduleItems([...scheduleItems, { week_day: 0, from: "", to: "" }]);
+  }
+
   return (
     <div id="page-teacher-form" className="container">
       <PageHeader
@@ -23,30 +31,61 @@ const TeacherForm: React.FC = () => {
           <Input label="Nome completo" name="name" />
           <Input label="Avatar" name="avatar" />
           <Input label="Whatsapp" name="whatsapp" />
-          <Textarea label="Biografia" name="bio"/>
+          <Textarea label="Biografia" name="bio" />
         </fieldset>
 
         <fieldset>
           <legend>Sobre a aula</legend>
 
-          <Select 
-            label="Matéria" 
+          <Select
+            label="Matéria"
             name="subject"
             options={[
-              { value: 'Artes', label: 'Artes' },
-              { value: 'Português', label: 'Português' },
-              { value: 'Matemática', label: 'Matemática' },
-              { value: 'Química', label: 'Química' },
-              { value: 'Física', label: 'Física' },
-              { value: 'Biologia', label: 'Biologia' },
-              { value: 'Geografia', label: 'Geografia' },
-              { value: 'História', label: 'História' },
-              { value: 'Educação Física', label: 'Educação Física' },
-              { value: 'Inglês', label: 'Inglês' },
-              { value: 'Espanhol', label: 'Espanhol' },
+              { value: "Artes", label: "Artes" },
+              { value: "Português", label: "Português" },
+              { value: "Matemática", label: "Matemática" },
+              { value: "Química", label: "Química" },
+              { value: "Física", label: "Física" },
+              { value: "Biologia", label: "Biologia" },
+              { value: "Geografia", label: "Geografia" },
+              { value: "História", label: "História" },
+              { value: "Educação Física", label: "Educação Física" },
+              { value: "Inglês", label: "Inglês" },
+              { value: "Espanhol", label: "Espanhol" },
             ]}
           />
           <Input label="Custo da sua hora por aula" name="cost" />
+        </fieldset>
+
+        <fieldset>
+          <legend>
+            Horários Disponíveis
+            <button onClick={addNewScheduleItem} type="button">
+              + Novo horário
+            </button>
+          </legend>
+
+          {scheduleItems.map((item) => {
+            return (
+              <div key={item.week_day} className="schedule-item">
+                <Select
+                  label="Dia da Semana"
+                  name="week_day"
+                  options={[
+                    { value: "0", label: "Domingo-feira" },
+                    { value: "1", label: "Segunda-feira" },
+                    { value: "2", label: "Terça-feira" },
+                    { value: "3", label: "Quarta-feira" },
+                    { value: "4", label: "Quinta-feira" },
+                    { value: "5", label: "Sexta-feira" },
+                    { value: "6", label: "Sábado-feira" },
+                  ]}
+                />
+                <Input name="from" label="Das" type="time" />
+                <Input name="to" label="Até" type="time" />
+              </div>
+            );
+          })}
         </fieldset>
 
         <footer>
@@ -56,9 +95,7 @@ const TeacherForm: React.FC = () => {
             Preencha todos os dados
           </p>
 
-          <button>
-            Salvar cadastro
-          </button>
+          <button>Salvar cadastro</button>
         </footer>
       </main>
     </div>
