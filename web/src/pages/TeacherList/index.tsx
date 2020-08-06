d8ae8062d0b52f1fd976ca7836fb97132ba711d9
Matheus Santos <matheus.psantos2016@gmail.com>
2020-08-06 12:12:27 -0300

    feat: Select component added

--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -5,15 +5,44 @@ import TeacherItem from "../../components/TeacherItem";
 
 import "./styles.css";
 import Input from "../../components/Input";
+import Select from "../../components/Select";
 
 const TeacherList: React.FC = () => {
   return (
     <div id="page-teacher-list" className="container">
       <PageHeader title="Estes são os proffys diponíveis.">
         <form id="search-teachers">
-          <Input label="Matéria" name="subject" />
+          <Select 
+            label="Matéria" 
+            name="subject" 
+            options={[
+              { value: 'Artes', label: 'Artes' },
+              { value: 'Português', label: 'Português' },
+              { value: 'Matemática', label: 'Matemática' },
+              { value: 'Química', label: 'Química' },
+              { value: 'Física', label: 'Física' },
+              { value: 'Biologia', label: 'Biologia' },
+              { value: 'Geografia', label: 'Geografia' },
+              { value: 'História', label: 'História' },
+              { value: 'Educação Física', label: 'Educação Física' },
+              { value: 'Inglês', label: 'Inglês' },
+              { value: 'Espanhol', label: 'Espanhol' },
+            ]}
+          />
 
-          <Input label="Dia da Semana" name="week_day" />
+          <Select 
+            label="Dia da Semana" 
+            name="week_day" 
+            options={[
+              { value: '0', label: 'Domingo-feira' },
+              { value: '1', label: 'Segunda-feira' },
+              { value: '2', label: 'Terça-feira' },
+              { value: '3', label: 'Quarta-feira' },
+              { value: '4', label: 'Quinta-feira' },
+              { value: '5', label: 'Sexta-feira' },
+              { value: '6', label: 'Sábado-feira' },
+            ]}
+          />
 
           <Input type="time" label="Hora" name="time" />
         </form>
