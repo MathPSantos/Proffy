92dc604de34e55acda640363ab4b02dcfe2e9666
Matheus Santos <matheus.psantos2016@gmail.com>
2020-08-08 18:51:19 -0300

    fix: style fixed

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -1,41 +1,56 @@
 import React from "react";
 
-import WhatsappIcon from '../../assets/images/icons/whatsapp.svg'
+import WhatsappIcon from "../../assets/images/icons/whatsapp.svg";
 
 import "./styles.css";
+import api from "../../services/api";
 
-const TeacherItem: React.FC = () => {
+export interface Teacher {
+  id: number;
+  name: string;
+  avatar: string;
+  bio: string;
+  cost: string;
+  subject: string;
+  whatsapp: string;
+}
+
+interface TeacherItemProps {
+  teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+  function createNewConnection() {
+    api.post('/connections', {
+      user_id: teacher.id
+    });
+  }
+
   return (
     <article className="teacher-item">
       <header>
-        <img
-          src="https://avatars1.githubusercontent.com/u/44923226?s=460&u=e5dec65272c221cbdcde1d237b0049e2236560b9&v=4"
-          alt="Matheus Pires Santos"
-        />
+        <img src={teacher.avatar} alt={teacher.name} />
         <div>
-          <strong>Matheus Pires Santos</strong>
-          <span>Matemática</span>
+          <strong>{teacher.name}</strong>
+          <span>{teacher.subject}</span>
         </div>
       </header>
 
-      <p>
-        Entusiasta das melhores tecnologias de química avançada.
-        <br />
-        <br />
-        Apaixonado por explodir coisas em laboratório e por mudar a vida das
-        pessoas através de experiências. de 200.000 pessoas já passaram por uma
-        das minhas explosões.
-      </p>
+      <p>{teacher.bio}</p>
 
       <footer>
         <p>
           Preço/hora
-          <strong>R$ 75,00</strong>
+          <strong>R$ {teacher.cost}</strong>
         </p>
-        <button>
+        <a 
+          target='blank'
+          href={`https://wa.me/${teacher.whatsapp}`}
+          onClick={createNewConnection} 
+        >
           <img src={WhatsappIcon} alt="Whatsapp" />
           Entrar em contato
-        </button>
+        </a>
       </footer>
     </article>
   );
